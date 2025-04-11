fc037caed36a75f497135584f2bee5caf36c53c8
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-04-11 19:59:29 +0300

    init - model TelegramUser

--- capybara_tg_user/models.py
+++ capybara_tg_user/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TelegramUser(AbstractUser):
+    telegram_id = models.BigIntegerField(unique=True, verbose_name='Telegram ID')
+    photo_url   = models.URLField(blank=True, null=True, verbose_name='Photo URL')
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'telegram_id']
+
+    class Meta:
+        verbose_name = 'Telegram User'
+        verbose_name_plural = 'Telegram Users'
+
+    def __str__(self):
+        return self.get_full_name() or self.username or str(self.telegram_id)
