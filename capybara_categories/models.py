47226f53f34d12b40620eef791ea3daad2efb29d
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-04-11 19:13:49 +0300

    init - model Category

--- capybara_categories/models.py
+++ capybara_categories/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="Catogory")
+    order = models.SmallIntegerField(default=0, db_index=True, verbose_name="Order")
+    slug = models.SlugField(max_length=50, unique=True, verbose_name="Slug")
+    image = models.ImageField(upload_to='media/images/cat_img/', blank=True, null=True, verbose_name="Image")
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = self.name.lower().replace(' ', '-')
+        super(Category, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("category:category_detail", kwargs={"pk": self.pk})
+    
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ['order']
