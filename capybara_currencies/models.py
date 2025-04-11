88ceace6a9325522f5dd591e2d7ae8d7f16debe7
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-04-11 16:54:58 +0300

    init - model Currency

--- capybara_currencies/models.py
+++ capybara_currencies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Currency(models.Model):
+    """
+    Модель для валют.
+    """
+    name = models.CharField(max_length=20, db_index=True, verbose_name="Название")
+    code = models.CharField(max_length=8, db_index=True, verbose_name="Код")
+    order = models.SmallIntegerField(default=0, db_index=True, verbose_name='Порядок')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Валюта"
+        verbose_name_plural = "Валюты"
+        ordering = ['order']
--- capybara_products/models.py
+++ capybara_products/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+from capybara_currencies.models import Currency
+
 from choices import STATUS_CHOICES
 from .utils_img import process_image
 
