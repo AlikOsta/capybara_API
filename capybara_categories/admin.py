e182b29ddec9008dd19b66b17f2c00a64cabfaeb
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-05-02 19:58:17 +0300

    изменение модели категорий + подкатегорий

--- capybara_categories/admin.py
+++ capybara_categories/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, SuperRubric, SubRubric
+from .forms import SubRubricForm
+
+
+class SubRubricInline(admin.TabularInline):
+    model = SubRubric
+
+
+class SuperRubricAdmin(admin.ModelAdmin):
+    exclude = ('super_rubric',)
+    inlines = [SubRubricInline]
+
+
+class SubRubricAdmin(admin.ModelAdmin):
+    form = SubRubricForm
+
 
 admin.site.register(Category)
+
+admin.site.register(SuperRubric, SuperRubricAdmin)
+
+
--- capybara_categories/models.py
+++ capybara_categories/models.py
@@ -29,3 +29,47 @@ class Category(models.Model):
         products = self.products.filter(status=3)
         return products.count()
     
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, unique=True, verbose_name="Rubric")
+    order = models.SmallIntegerField(default=0, db_index=True, verbose_name="Order")
+    slug = models.SlugField(max_length=30, verbose_name="Slug")
+    super_rubric = models.ForeignKey('SuperRubric', on_delete=models.PROTECT, null=True, blank=True, related_name='sub_rubrics', verbose_name="Super Rubric")
+    image = models.ImageField(upload_to='images/rubric_img/')
+
+
+class SuperRubricManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(super_rubric__isnull=True)
+    
+
+class SuperRubric(Rubric):
+    objects = SuperRubricManager()
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        proxy = True
+        ordering = ['order', 'name']
+        verbose_name = 'Super Rubric'
+        verbose_name_plural = 'Super Rubrics'
+
+
+class SubRubricManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(super_rubric__isnull=False)
+    
+
+class SubRubric(Rubric):
+    objects = SubRubricManager()
+
+    def __str__(self)-> str:
+        return f'{self.super_rubric.name} - {self.name}'
+
+    class Meta:
+        proxy = True
+        ordering = ['super_rubric__order', 'super_rubric__name', 'order', 'name']
+        verbose_name = 'Sub Rubric'
+        verbose_name_plural = 'Sub Rubrics'
